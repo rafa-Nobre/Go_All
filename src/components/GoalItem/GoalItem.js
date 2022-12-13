7c2ffbbbca459205a67cc422d0e67d48e3c3feaf
rafa-Nobre <denys.nobre@outlook.com>
2022-12-13 12:01:26 -0300

    Styling features in the item

--- src/components/GoalItem/GoalItem.js
+++ src/components/GoalItem/GoalItem.js
@@ -2,21 +2,31 @@ import { View, Text, Pressable, StyleSheet } from 'react-native'
 
 const GoalItem = props => {
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      <View style={styles.goalWrapper}>
-        <Text style={{ color: 'white' }}>{props.text}</Text>
-      </View>
-    </Pressable>
+    <View style={styles.goalWrapper}>
+      <Pressable
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        android_ripple={{ color: '#210644' }}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
+        <Text style={styles.text}>{props.text}</Text>
+      </Pressable>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
   goalWrapper: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: '#5e0acc',
     color: 'white'
+  },
+  text: {
+    padding: 8,
+    color: 'white'
+  },
+  pressedItem: {
+    opacity: 0.5
   }
 })
 
