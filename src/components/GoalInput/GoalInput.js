fa56878f9bdd84206a2dba87faed7050767806a0
rafa-Nobre <denys.nobre@outlook.com>
2022-12-13 12:19:49 -0300

    Styling upgrade

--- src/components/GoalInput/GoalInput.js
+++ src/components/GoalInput/GoalInput.js
@@ -21,7 +21,14 @@ const GoalInput = props => {
           value={enteredGoalText}
           style={styles.textInput}
         />
-        <Button title="Add Goal" onPress={addGoalHandler} />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="Add Goal" onPress={addGoalHandler} />
+          </View>
+          <View style={styles.button}>
+            <Button title="Cancel" />
+          </View>
+        </View>
       </View>
     </Modal>
   )
@@ -30,19 +37,26 @@ const GoalInput = props => {
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 24,
+    padding: 16,
     borderBottomWidth: 1,
     borderBottomColor: '#cccccc'
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '70%',
-    marginRight: 8,
+    width: '100%',
     padding: 8
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 16
+  },
+  button: {
+    width: 100,
+    marginHorizontal: 8
   }
 })
 
